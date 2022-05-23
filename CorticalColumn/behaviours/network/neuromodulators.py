be20f19512161fd11655edcd1122c7265842dc16
atenagm1375 <ashenatena@gmail.com>
2022-05-23 11:11:39 +0430

    add reward and dopamine

--- CorticalColumn/behaviours/network/neuromodulators.py
+++ CorticalColumn/behaviours/network/neuromodulators.py
@@ -1,3 +1,43 @@
 """
 Network-wide neuromodulators.
 """
+
+from PymoNNto import Behaviour
+
+
+class Dopamine(Behaviour):
+    """
+    Compute extracellular dopamine concentration.
+
+    Note: Reward behavior should be defined prior to Dopamine behavior
+    while defining the network.
+
+    Args:
+        tau_dopamine (float): Dopamine decay time constant.
+    """
+
+    def set_variables(self, network):
+        """
+        Set initial dopamine concentration value based on initial reward value.
+
+        Args:
+            network (Network): Network object.
+        """
+        self.add_tag("Dopamine")
+        self.set_init_attrs_as_variables(network)
+
+        network.dopamine_concentration = network.reward
+
+    def new_iteration(self, network):
+        """
+        Compute extracellular dopamine concentration at each time step by:
+
+        dd/dt = -d/tau_d + reward(t).
+
+        Args:
+            network (Network): Network object.
+        """
+        dd_dt = (
+            -(network.dopamine_concentration / network.tau_dopamine) + network.reward
+        )
+        network.dopamine_concentration += dd_dt
---
+++ CorticalColumn/behaviours/network/reward.py
@@ -0,0 +1,26 @@
+"""
+Reward definition base.
+"""
+
+from PymoNNto import Behaviour
+
+
+class Reward(Behaviour):
+    """
+    Base behavior class to define reward function. Define the desired reward
+    function by inheriting this class and defining `new_iteration` abstract
+    method per se. You will set `network.reward` with the reward value at the
+    time step.
+
+    Args:
+        initial_reward (float): Initial reward value. Default is 0.0.
+    """
+
+    def set_variables(self, network):
+        """
+        Initialize network's reward with `initial_reward`.
+
+        Args:
+            network (Network): Network object.
+        """
+        network.reward = self.get_init_attr("initial_reward", 0.0, network)
