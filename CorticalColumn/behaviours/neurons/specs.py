c04774a79d2070fc39fe434d598a520f549ba5e4
atenagm1375 <ashenatena@gmail.com>
2022-09-07 11:11:36 +0430

    add KWTA

--- CorticalColumn/behaviours/neurons/specs.py
+++ CorticalColumn/behaviours/neurons/specs.py
@@ -2,8 +2,9 @@
 General specifications needed for spiking neurons.
 """
 
+import numpy as np
 from PymoNNto import Behaviour
-# TODO: add KWTA behavior.
+
 
 class Fire(Behaviour):
     """
@@ -16,6 +17,44 @@ class Fire(Behaviour):
         neurons.spikes = neurons.v >= neurons.threshold
         neurons.v[neurons.spikes] = neurons.v_reset
         neurons.effective_spikes = neurons.spikes.copy()
+
+
+class KWTA(Behaviour):
+    """
+    KWTA behavior of spiking neurons:
+
+    if v >= threshold then v = v_reset and all other neurons are inhibited.
+    """
+
+    def __calc_inhibited(self, neurons, winners_masked):
+        """
+        Calculates the inhibition rate for each neuron.
+        """
+        max_sub_threshold = neurons.v[neurons.v < neurons.threshold].max()
+        val = neurons.threshold - max_sub_threshold
+        sup_threshold = winners_masked[
+            (~winners_masked.mask) & (winners_masked >= neurons.threshold)
+        ]
+        return (
+            val * (sup_threshold - sup_threshold.min()) / sup_threshold.ptp()
+            - max_sub_threshold
+        )
+
+    def set_variables(self, neurons):
+        self.k = self.get_init_attr("k", 10)
+
+    def new_iteration(self, neurons):
+        will_spike = neurons.v * (neurons.v >= neurons.threshold)
+
+        if will_spike.sum() <= self.k:
+            return
+
+        kw = np.argpartition(will_spike, -self.k)[-self.k :]
+        winners_masked = np.ma.array(neurons.v, mask=False)
+        winners_masked.mask[kw] = True
+        winners_masked[
+            (~winners_masked.mask) & (winners_masked >= neurons.threshold)
+        ] = self.__calc_inhibited(neurons, winners_masked)
 
 
 class SpikeTrace(Behaviour):
