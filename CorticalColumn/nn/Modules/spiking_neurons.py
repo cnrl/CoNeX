0cb9e1248a1ddb82109112d817521a846d2a9fb8
atenagm1375 <ashenatena@gmail.com>
2023-03-12 13:07:45 +0330

    modify SpikingNeuronGroup based on latest changes and make it more user-friendly

--- CorticalColumn/nn/Modules/spiking_neurons.py
+++ CorticalColumn/nn/Modules/spiking_neurons.py
@@ -2,45 +2,58 @@
 Structure of spiking neural populations.
 """
 
+import warnings
+from CorticalColumn.behaviours.neurons.lif_neurons import LIF
+from CorticalColumn.behaviours.neurons.specs import KWTA, Fire, InherentNoise, NeuronAxon, NeuronDendrite, SpikeTrace
 from pymonntorch import NeuronGroup
 
-from CorticalColumn.behaviours import Fire
-
+import torch
 
 class SpikingNeuronGroup(NeuronGroup):
     def __init__(
         self,
         size,
-        behaviour,
         net,
+        neuron_type=LIF,
+        kwta=1,
+        kwta_dim=None,
+        tau_trace=None,
+        max_delay=None,
+        noise_function=None,
         tag=None,
         color=None,
-        v_rest=0.0,
-        v_reset=0.0,
-        threshold=10.0,
-        has_noise=True,
+        dendrite_params={},
+        neuron_params={},
     ):
-        assert 2 not in behaviour, "Behaviour index 1 is reserved for firing behaviour."
-
         if tag is None and net is not None:
             tag = "SpikingNeuronGroup_" + str(len(net.NeuronGroups) + 1)
 
-        super().__init__(size, behaviour, net, tag, color)
+        behavior = {
+            31 : NeuronDendrite(**dendrite_params),
+            32 : neuron_type(**neuron_params),
+            34 : KWTA(k=kwta, dimension=kwta_dim),
+            35 : Fire(),
+            38 : NeuronAxon(max_delay=max_delay)
+        }
 
-        self.v_rest = v_rest * self.get_neuron_vec(mode="ones()")
-        self.v_reset = v_reset * self.get_neuron_vec(mode="ones()")
-        self.threshold = threshold * self.get_neuron_vec(mode="ones()")
+        if tau_trace:
+            behavior[37] = SpikeTrace(tau_s=tau_trace)
+        
+        if noise_function:
+            behavior[33] = InherentNoise(noise_function)
 
-        self.v = self.v_rest * self.get_neuron_vec(mode="ones()")
-        self.spikes = self.get_neuron_vec(mode="zeros()")
+        super().__init__(size, behavior, net, tag, color)
 
-        net.add_behaviours_to_object({2: Fire()}, self)
-
-        if has_noise:
-            self.noisy_current = self.get_neuron_vec(mode="uniform(0.0, 1.0)")
-        else:
-            self.noisy_current = None
-
-        self.proximal_input_current = self.get_neuron_vec(mode="zeros()")
-        self.basal_input_current = self.get_neuron_vec(mode="zeros()")
-        self.apical_input_current = self.get_neuron_vec(mode="zeros()")
+        if not hasattr(self, "v"):
+            if hasattr(self, "v_rest"):
+                warnings.warn("Spiking neuron behavior lacks attribute v. Adding the attribute...")
+                self.v = self.v_rest * self.get_neuron_vec(mode="ones()")
+            else:
+                raise AttributeError("Spiking neuron lacks attribute v.")
+            
+        if not hasattr(self, "spikes"):
+            warnings.warn("Spiking neuron behavior lacks attribute spike. Adding the attribute...")
+            if hasattr(self, "init_s"):
+                if isinstance(self.init_s, torch.Tensor):
+                    self.spikes = self.init_s
+            self.spikes = self.get_neuron_vec(mode="zeros()")
