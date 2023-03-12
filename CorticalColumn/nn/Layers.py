bba7d21a7e078f3ca7cd6533437eebbf3a5914fd
atenagm1375 <ashenatena@gmail.com>
2023-03-12 16:06:10 +0330

    add superclass for defining the layers

--- CorticalColumn/nn/Layers.py
+++ CorticalColumn/nn/Layers.py
@@ -1,454 +1,91 @@
 """
 Implementation of Cortical layers.
 """
-from CorticalColumn.behaviours.neurons.lif_neurons import AELIF, LIF
-from CorticalColumn.behaviours.neurons.specs import KWTA, SpikeHistory, SpikeTrace
-from CorticalColumn.behaviours.synapses.dendrites import (
-    ApicalInput,
-    BasalInput,
-    ProximalInput,
-)
-from CorticalColumn.behaviours.synapses.learning import STDP
-from CorticalColumn.behaviours.synapses.specs import WeightClip
-from CorticalColumn.nn.Modules.spiking_neurons import SpikingNeuronGroup
-from CorticalColumn.nn.Modules.topological_connections import SparseSynapseGroup
 
 # TODO: Complete implementation of each layer, including the undefined methods and attributes.
 
 
-class L2_3:
+import warnings
+from CorticalColumn.nn.Modules.spiking_neurons import SpikingNeuronGroup
+from CorticalColumn.nn.Modules.topological_connections import StructuredSynapseGroup
+from pymonntorch import NeuronGroup, SynapseGroup
+
+
+class Layer:
     def __init__(
         self,
-        exc_shape,
-        inh_shape,
         net,
-        exc_exc_rate=0.2,
-        exc_inh_rate=0.4,
-        inh_exc_rate=0.4,
-        inh_inh_rate=0.1,
+        exc_pop_config=None,
+        inh_pop_config=None,
+        exc_exc_config=None,
+        exc_inh_config=None,
+        inh_exc_config=None,
+        inh_inh_config=None,
     ):
-        self.exc_pop = SpikingNeuronGroup(
-            exc_shape,
-            behaviour={
-                1: KWTA(k=10),
-                3: AELIF(tau=20, r=1, delta=2, theta_rh=15, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=20,
-            tau_s=40.0,
-        )
-        self.inh_pop = SpikingNeuronGroup(
-            inh_shape,
-            behaviour={
-                3: LIF(tau=20, r=1, delta=2, theta_rh=6, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=10,
-            tau_s=30.0,
-        )
+        self.exc_pop = self._create_neural_population(net, exc_pop_config)
+        if self.exc_pop and "exc" not in self.exc_pop.tags:
+                self.exc_pop.add_tag("exc")
 
-        self.exc_exc_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.exc_pop,
-            net,
-            exc_exc_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.005, a_minus=0.002),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.exc_inh_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.inh_pop,
-            net,
-            exc_inh_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.05, a_minus=0.02),
-                7: WeightClip(w_min=0.0, w_max=1.0),
-            },
-        )
-        self.inh_exc_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.exc_pop,
-            net,
-            inh_exc_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.02, a_minus=0.01),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.inh_inh_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.inh_pop,
-            net,
-            inh_inh_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.002, a_minus=0.001),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
+        self.inh_pop = self._create_neural_population(net, inh_pop_config)
+        if self.inh_pop and "exc" not in self.inh_pop.tags:
+                self.inh_pop.add_tag("inh")
+
+        if self.inh_pop is None and self.exc_pop is None:
+            raise RuntimeError(f"No proper neural population defined for {self.__class__.__name__}")
+        
+        if self.exc_pop:
+            self.exc_exc_syn = self._create_synaptic_connection(
+                self.exc_pop, self.exc_pop, net, exc_exc_config
+            )
+            if self.exc_exc_syn:
+                self.exc_exc_syn.add_tag("Proximal")
+        
+        if self.inh_pop:
+            self.inh_inh_syn = self._create_synaptic_connection(
+                self.inh_pop, self.inh_pop, net, inh_inh_config
+            )
+            if self.inh_inh_syn:
+                self.inh_inh_syn.add_tag("Proximal")
+        
+        if self.inh_pop and self.exc_pop:
+            self.exc_inh_syn = self._create_synaptic_connection(
+                self.exc_pop, self.inh_pop, net, exc_inh_config
+            )
+            if self.exc_inh_syn:
+                self.exc_inh_syn.add_tag("Proximal")
+            
+            self.inh_exc_syn = self._create_synaptic_connection(
+                self.inh_pop, self.exc_pop, net, inh_exc_config
+            )
+            if self.inh_exc_syn:
+                self.inh_exc_syn.add_tag("Proximal")
+
+            if self.inh_exc_syn is None and self.exc_inh_syn is None:
+                raise RuntimeError(f"No connection between Excitatory and Inhibitory populations in {self.__class__.__name__}")
+            
+    @classmethod
+    def _create_neural_population(cls, net, config):
+        if  isinstance(config, dict):
+            if not config.get("user_defined", False):
+                return SpikingNeuronGroup(net=net, **config)
+            else:
+                return NeuronGroup(net=net, **config)
+        elif isinstance(config, NeuronGroup):
+            return config
+        else:
+            warnings.warn(f"No proper excitatory population is defined in {cls.__name__}.")
+            return None
+        
+    @classmethod
+    def _create_synaptic_connection(cls, src, dst, net, config):
+        if isinstance(config, dict):
+            if not config.get("user_defined", False):
+                return StructuredSynapseGroup(src, dst, net, config)
+            else:
+                return SynapseGroup(src, dst, net, config)
+        else:
+            warnings.warn(f"No synaptic connection from {src} to {dst} in {cls.__name__}")
+            return None
 
     def forward(self, input):
-        pass
-
-    def backward(self, delta):
-        pass
-
-    def update(self, learning_rate):
-        pass
-
-
-class L4:
-    def __init__(
-        self,
-        exc_shape,
-        inh_shape,
-        net,
-        exc_exc_rate=0.2,
-        exc_inh_rate=0.4,
-        inh_exc_rate=0.4,
-        inh_inh_rate=0.1,
-    ):
-        self.exc_pop = SpikingNeuronGroup(
-            exc_shape,
-            behaviour={
-                3: AELIF(tau=20, r=1, delta=2, theta_rh=15, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=20,
-            tau_s=40.0,
-        )
-        self.inh_pop = SpikingNeuronGroup(
-            inh_shape,
-            behaviour={
-                3: LIF(tau=20, r=1, delta=2, theta_rh=6, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=10,
-            tau_s=30.0,
-        )
-
-        self.exc_exc_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.exc_pop,
-            net,
-            exc_exc_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.005, a_minus=0.002),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.exc_inh_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.inh_pop,
-            net,
-            exc_inh_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.05, a_minus=0.02),
-                7: WeightClip(w_min=0.0, w_max=1.0),
-            },
-        )
-        self.inh_exc_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.exc_pop,
-            net,
-            inh_exc_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.02, a_minus=0.01),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.inh_inh_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.inh_pop,
-            net,
-            inh_inh_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.002, a_minus=0.001),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-
-    def forward(self, input):
-        pass
-
-    def backward(self, delta):
-        pass
-
-    def update(self, learning_rate):
-        pass
-
-
-# TODO: Should L5 and L6 be combined?
-class L5:
-    def __init__(
-        self,
-        exc_shape,
-        inh_shape,
-        net,
-        exc_exc_rate=0.2,
-        exc_inh_rate=0.4,
-        inh_exc_rate=0.4,
-        inh_inh_rate=0.1,
-    ):
-        self.exc_pop = SpikingNeuronGroup(
-            exc_shape,
-            behaviour={
-                3: LIF(tau=20, r=1, delta=2, theta_rh=15, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=20,
-            tau_s=40.0,
-        )
-        self.inh_pop = SpikingNeuronGroup(
-            inh_shape,
-            behaviour={
-                3: LIF(tau=20, r=1, delta=2, theta_rh=6, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=10,
-            tau_s=30.0,
-        )
-
-        self.exc_exc_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.exc_pop,
-            net,
-            exc_exc_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.005, a_minus=0.002),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.exc_inh_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.inh_pop,
-            net,
-            exc_inh_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.05, a_minus=0.02),
-                7: WeightClip(w_min=0.0, w_max=1.0),
-            },
-        )
-        self.inh_exc_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.exc_pop,
-            net,
-            inh_exc_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.02, a_minus=0.01),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.inh_inh_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.inh_pop,
-            net,
-            inh_inh_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.002, a_minus=0.001),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-
-    def forward(self, input):
-        pass
-
-    def backward(self, delta):
-        pass
-
-    def update(self, learning_rate):
-        pass
-
-
-class L6:
-    def __init__(
-        self,
-        exc_shape,
-        inh_shape,
-        net,
-        exc_exc_rate=0.2,
-        exc_inh_rate=0.4,
-        inh_exc_rate=0.4,
-        inh_inh_rate=0.1,
-    ):
-        self.exc_pop = SpikingNeuronGroup(
-            exc_shape,
-            behaviour={
-                3: LIF(tau=20, r=1, delta=2, theta_rh=15, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=20,
-            tau_s=40.0,
-        )
-        self.inh_pop = SpikingNeuronGroup(
-            inh_shape,
-            behaviour={
-                3: LIF(tau=20, r=1, delta=2, theta_rh=6, alpha=1, beta=1.5),
-                4: SpikeTrace(),
-                5: SpikeHistory(),
-            },
-            net=net,
-            v_rest=0.0,
-            v_reset=-2.0,
-            threshold=10,
-            tau_s=30.0,
-        )
-
-        self.exc_exc_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.exc_pop,
-            net,
-            exc_exc_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.005, a_minus=0.002),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.exc_inh_conn = SparseSynapseGroup(
-            self.exc_pop,
-            self.inh_pop,
-            net,
-            exc_inh_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.05, a_minus=0.02),
-                7: WeightClip(w_min=0.0, w_max=1.0),
-            },
-        )
-        self.inh_exc_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.exc_pop,
-            net,
-            inh_exc_rate,
-            weight_init_mode="normal(0.5, 0.5)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.02, a_minus=0.01),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-        self.inh_inh_conn = SparseSynapseGroup(
-            self.inh_pop,
-            self.inh_pop,
-            net,
-            inh_inh_rate,
-            weight_init_mode="normal(0.25, 0.25)",
-            delay_init_mode="normal(0.5, 0.5)",
-            behaviour={
-                3: ApicalInput(),
-                4: BasalInput(),
-                5: ProximalInput(),
-                6: STDP(a_plus=0.002, a_minus=0.001),
-                7: WeightClip(w_min=0.0, w_max=0.5),
-            },
-        )
-
-    def forward(self, input):
-        pass
-
-    def backward(self, delta):
-        pass
-
-    def update(self, learning_rate):
         pass
