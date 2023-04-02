d517ca40b07e33abab5670e2948694767bf418a5
atenagm1375 <ashenatena@gmail.com>
2023-04-02 09:55:15 +0330

    add usage instruction

--- CCSNN/nn/Structure/Neocortex.py
+++ CCSNN/nn/Structure/Neocortex.py
@@ -13,6 +13,101 @@ class Neocortex(Network):
         reward (Reward): If not None, enables reinforcement learning with a reward function defined by an instance of Reward class.
         neuromodulators (list): List of Neuromodulators used in the network.
         device (str): Device on which the network and its components are located. The default is "cpu".
+
+    Usage:
+        net = Neocortex()
+
+        # define neural population that holds processed sensory input spikes
+        sensory_inp = NeuronGroup(size=28*28, net=net, behavior={...})
+
+        # define neural network responsible for generating output
+        output = SpikingNeuronGroup(
+            10, 
+            net, 
+            neuron_type=LIF,
+            kwta=1,
+            dendrite_params={
+                "proximal_max_delay": 2
+            },
+            neuron_params={
+                "tau": 20,
+                "R": 1,
+                "threshold": -55,
+                "v_rest": -70,
+                "v_reset": -72
+            }
+        )
+
+        # define a cortical column by specifying the configuration of its layers and their connections
+        cc1 = CorticalColumn(
+            net,
+            sensory_inp,
+            representation_layer=output,
+            L2_3_config={
+                "exc_pop_config": {
+                    "neuron_type": AELIF,
+                    "kwta": 5,
+                    "dendrite_params": {...},
+                    "neuron_params": {...},
+                },
+                "inh_pop_config": {
+                    "neuron_type": ELIF,
+                    "neuron_params": {...},
+                    "dendrite_params": {...},
+                },
+                "exc_inh_config": {
+                    "structure": "Simple",
+                    "learning_rule": None,
+                    "weight_init_params": {
+                        "mode": "randn",
+                    }
+                }
+                "inh_exc_config": {
+                    "structure": "Simple",
+                    "learning_rule": None,
+                    "weight_init_params": {
+                        "mode": "randn",
+                    }
+                }
+            }
+            L4_config={...},
+            L4_L2_3_config={
+                "exc_exc": {
+                    "structure": "Conv2d",
+                    "learning_rule": "STDP",
+                    "weight_init_params": {
+                        "mode": "randn",
+                    }
+                }
+            }
+            sensory_L4_config={...},
+            L2_3_representation_config={...},
+        )
+
+        # The same can go here for creating another cortical column
+        cc2 = CorticalColumn(...)
+
+        # specify the connectivity configurations of the two columns
+        # cc1 -> cc2
+        cc1.connect(
+            cc2,
+            L2_3_L2_3_config={
+                "exc_exc": {...},
+                "inh_exc": {...}
+            }
+        )
+        # cc2 -> cc1
+        cc2.connect(
+            cc1,
+            L2_3_L2_3_config={
+                "exc_exc": {...},
+                "inh_exc": {...}
+            }
+        )
+
+        net.initialize()
+
+        # Now you can simulate your network using net.simulate_iterations(...)
     """
 
     def __init__(self, dt=1, reward=None, neuromodulators=[], device="cpu"):
