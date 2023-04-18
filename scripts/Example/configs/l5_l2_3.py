2378f2b0780384ef0f8658cc7f395054f9fd4044
Saeed <saeed.ali.rahimi@gmail.com>
2023-04-18 12:51:11 +0330

    removes bad Example

--- scripts/Example/configs/l5_l2_3.py
+++
@@ -1,7 +0,0 @@
-l5_l2_3 = {"exc_exc": { "src_pop" : "exc_pop",
-                        "dst_pop" : "exc_pop",
-                        "structure": "Simple",
-                        "weight_init_params": {"mode": "uniform"},
-                        "structure_params": {"current_coef": 3},
-                      }           
-          }
