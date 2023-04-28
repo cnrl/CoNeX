81f738308d4c24581d168cdece1af51a818dfae5
Saeed <saeed.ali.rahimi@gmail.com>
2023-04-28 06:39:50 +0330

    Remove extra layers from cortical column

--- conex/nn/Structure/CorticalColumn.py
+++ conex/nn/Structure/CorticalColumn.py
@@ -21,55 +21,32 @@ class CorticalColumn(TaggableObject):
 
     Args:
         net (Neocortex): The cortical network the column belongs to.
-        sensory_layer (NeuronGroup): The neural population representing the sensory input.
-        location_layer (NeuronGroup): If not None, specifies the neural population representing the location signal input.
-        representation_layer (NeuronGroup): If not None, indicates the neural population representing the output representation.
-        motor_layer (NeuronGroup): If not None, indicates the neural population representing motor output.
         L2_3_config (dict): If not None, adds L2/3 with the specified configurations to the column.
         L4_config (dict): If not None, adds L4 with the specified configurations to the column.
         L5_config (dict): If not None, adds L5 with the specified configurations to the column.
         L6_config (dict): If not None, adds L6 with the specified configurations to the column.
-        sensory_L4_syn_config (dict): If not None, adds the synaptic connections from sensory layer to L4 with the specified configurations.
-        sensory_L6_syn_config (dict): If not None, adds the synaptic connections from sensory layer to L6 with the specified configurations.
-        location_L6_syn_config (dict): If not None, adds the synaptic connections from location layer to L6 with the specified configurations.
         L6_L4_syn_config (dict): If not None, adds the synaptic connections from L6 to L4 with the specified configurations.
         L4_L2_3_syn_config (dict): If not None, adds the synaptic connections from L4 to L2/3 with the specified configurations.
         L2_3_L5_syn_config (dict): If not None, adds the synaptic connections from L2/3 to L5 with the specified configurations.
         L5_L2_3_syn_config (dict): If not None, adds the synaptic connections from L5 to L2/3 with the specified configurations.
         L5_L6_syn_config (dict): If not None, adds the synaptic connections from L5 to L6 with the specified configurations.
-        L2_3_representation_syn_config (dict): If not None, adds the synaptic connections from L2/3 to representation layer with the specified configurations.
-        L5_motor_syn_config (dict): If not None, adds the synaptic connections from L5 to motor layer with the specified configurations.
     """
 
     def __init__(
         self,
         net,
-        sensory_layer,
-        location_layer=None,
-        representation_layer=None,
-        motor_layer=None,
         L2_3_config=None,
         L4_config=None,
         L5_config=None,
         L6_config=None,
-        sensory_L4_syn_config=None,
-        sensory_L6_syn_config=None,
-        location_L6_syn_config=None,
         L6_L4_syn_config=None,
         L4_L2_3_syn_config=None,
         L2_3_L5_syn_config=None,
         L5_L2_3_syn_config=None,
         L5_L6_syn_config=None,
-        L2_3_representation_syn_config=None,
-        L5_motor_syn_config=None,
     ):
         super().__init__(f"CorticalColumn{len(net.columns)}", device=net.device)
         self.network = net
-
-        self.sensory_layer = sensory_layer
-        self.location_layer = location_layer
-        self.representation_layer = representation_layer
-        self.motor_layer = motor_layer
 
         self.L2_3 = self._create_layer(net, L2_3_config, "L2_3")
         self.L4 = self._create_layer(net, L4_config, "L4")
@@ -87,18 +64,6 @@ class CorticalColumn(TaggableObject):
             and self.L6 is None
         ):
             raise RuntimeError("No layers defined in the cortical column.")
-
-        self.sensory_L4_synapses = self._add_synaptic_connection(
-            self.sensory_layer, self.L4, sensory_L4_syn_config
-        )
-
-        self.sensory_L6_synapses = self._add_synaptic_connection(
-            self.sensory_layer, self.L6, sensory_L6_syn_config
-        )
-
-        self.location_L6_synapses = self._add_synaptic_connection(
-            self.location_layer, self.L6, location_L6_syn_config
-        )
 
         self.L6_L4_synapses = self._add_synaptic_connection(
             self.L6, self.L4, L6_L4_syn_config
@@ -120,14 +85,6 @@ class CorticalColumn(TaggableObject):
             self.L5, self.L6, L5_L6_syn_config
         )
 
-        self.L2_3_representation_synapses = self._add_synaptic_connection(
-            self.L2_3, self.representation_layer, L2_3_representation_syn_config
-        )
-
-        self.L5_motor_synapses = self._add_synaptic_connection(
-            self.L5, self.motor_layer, L5_motor_syn_config
-        )
-
         self.network.columns.append(self)
 
     def _create_layer(self, net, config, name):
@@ -138,6 +95,21 @@ class CorticalColumn(TaggableObject):
 
     @classmethod
     def _add_synaptic_connection(cls, src, dst, config):
+        # TODO fill the docstring
+        """_summary_
+
+        Args:
+            src (Layer or NeuronGroup): _description_
+            dst (Layer or NeuronGroup): _description_
+            config (Dictionary): _description_
+
+        Raises:
+            ValueError: _description_
+            ValueError: _description_
+
+        Returns:
+            _type_: _description_
+        """
         if src is None or dst is None:
             return {}
 
@@ -155,6 +127,7 @@ class CorticalColumn(TaggableObject):
             if pop_tag := config[key].get("dst_pop"):
                 dst_tag = dst_tag + "_" + pop_tag[:3]
             tag = f"{src_tag} => {dst_tag}"
+
             if isinstance(config[key], dict):
                 if isinstance(src, NeuronGroup):
                     src_pop = src
@@ -166,35 +139,32 @@ class CorticalColumn(TaggableObject):
                 else:
                     dst_pop = getattr(dst, config[key].pop("dst_pop"))
 
-                if not config[key].get("user_defined", False):
-                    synapses[key] = StructuredSynapseGroup(
-                        src=src_pop, dst=dst_pop, net=net, **config[key]
-                    )
-                else:
-                    synapses[key] = SynapseGroup(
-                        src=src_pop, dst=dst_pop, net=net, **config[key]
-                    )
-
+                synapses[key] = StructuredSynapseGroup(
+                    src=src_pop, dst=dst_pop, net=net, **config[key]
+                )
                 synapses[key].tags.insert(0, tag)
                 synapses[key].add_tag(key)
-                if hasattr(dst_pop, "cortical_column"):
-                    if src_pop.cortical_column == dst_pop.cortical_column:
-                        if "L4" in src_tag and "L2_3" in dst_tag:
-                            synapses[key].add_tag("Proximal")
+
+                if not (
+                    config[key].get("tag", None)
+                    and any(
+                        connection
+                        in list(map(str.strip, config[key]["tag"].split(",")))
+                        for connection in ["Proximal", "Distal", "Apical"]
+                    )
+                ):
+                    if hasattr(dst_pop, "cortical_column"):
+                        if src_pop.cortical_column == dst_pop.cortical_column:
+                            if "L4" in src_tag and "L2_3" in dst_tag:
+                                synapses[key].add_tag("Proximal")
+                            else:
+                                synapses[key].add_tag("Distal")
                         else:
-                            synapses[key].add_tag("Distal")
+                            synapses[key].add_tag("Apical")
+                    elif isinstance(dst_pop, OutputLayer):
+                        synapses[key].add_tag("Proximal")
                     else:
-                        synapses[key].add_tag("Apical")
-                elif isinstance(dst_pop, OutputLayer):
-                    synapses[key].add_tag("Proximal")
-                else:
-                    raise ValueError(f"Invalid destination object: {type(dst_pop)}")
-            elif isinstance(config[key], SynapseGroup) and config[key].network == net:
-                synapses[key] = config[key]
-
-                synapses[key].tags.insert(0, tag)
-                synapses[key].add_tag("Distal")
-                synapses[key].add_tag(key)
+                        raise ValueError(f"Invalid destination object: {type(dst_pop)}")
             else:
                 warnings.warn(
                     f"Ignoring connection {key} from {src.tags[0]} to {dst.tags[0]}..."
@@ -204,10 +174,10 @@ class CorticalColumn(TaggableObject):
     def connect(
         self,
         cortical_column,
-        L2_3_L2_3_config={},
-        L2_3_L4_config={},
-        L5_L5_config={},
-        L5_L6_config={},
+        L2_3_L2_3_config=None,
+        L2_3_L4_config=None,
+        L5_L5_config=None,
+        L5_L6_config=None,
     ):
         """
         Makes connections between current cortical column and another one.
@@ -223,29 +193,35 @@ class CorticalColumn(TaggableObject):
             L6_L6_config (dict): Adds the synaptic connections from L6 of current column to L6 of the other with the specified configurations.
         """
         synapses = {}
-        tag = self.tags[0] + "_" + "L2_3 => " + cortical_column.tags[0] + "_L2_3"
-        synapses[tag] = self._add_synaptic_connection(
-            self.L2_3, cortical_column.L2_3, L2_3_L2_3_config
-        )
-        all_empty = synapses[tag] == {}
+        all_empty = True
 
-        tag = self.tags[0] + "_" + "L2_3 => " + cortical_column.tags[0] + "_L4"
-        synapses[tag] = self._add_synaptic_connection(
-            self.L2_3, cortical_column.L4, L2_3_L4_config
-        )
-        all_empty *= synapses[tag] == {}
+        if L2_3_L2_3_config:
+            tag = self.tags[0] + "_" + "L2_3 => " + cortical_column.tags[0] + "_L2_3"
+            synapses[tag] = self._add_synaptic_connection(
+                self.L2_3, cortical_column.L2_3, L2_3_L2_3_config
+            )
+            all_empty *= synapses[tag] == {}
 
-        tag = self.tags[0] + "_" + "L5 => " + cortical_column.tags[0] + "_L5"
-        synapses[tag] = self._add_synaptic_connection(
-            self.L5, cortical_column.L5, L5_L5_config
-        )
-        all_empty *= synapses[tag] == {}
+        if L2_3_L4_config:
+            tag = self.tags[0] + "_" + "L2_3 => " + cortical_column.tags[0] + "_L4"
+            synapses[tag] = self._add_synaptic_connection(
+                self.L2_3, cortical_column.L4, L2_3_L4_config
+            )
+            all_empty *= synapses[tag] == {}
 
-        tag = self.tags[0] + "_" + "L5 => " + cortical_column.tags[0] + "_L6"
-        synapses[tag] = self._add_synaptic_connection(
-            self.L5, cortical_column.L6, L5_L6_config
-        )
-        all_empty *= synapses[tag] == {}
+        if L5_L5_config:
+            tag = self.tags[0] + "_" + "L5 => " + cortical_column.tags[0] + "_L5"
+            synapses[tag] = self._add_synaptic_connection(
+                self.L5, cortical_column.L5, L5_L5_config
+            )
+            all_empty *= synapses[tag] == {}
+
+        if L5_L6_config:
+            tag = self.tags[0] + "_" + "L5 => " + cortical_column.tags[0] + "_L6"
+            synapses[tag] = self._add_synaptic_connection(
+                self.L5, cortical_column.L6, L5_L6_config
+            )
+            all_empty *= synapses[tag] == {}
 
         if all_empty:
             raise RuntimeError(
