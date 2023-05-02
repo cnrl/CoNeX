335213936f5b0a848a33695a2bfb59df3967bf69
Saeed <saeed.ali.rahimi@gmail.com>
2023-05-02 18:20:05 +0330

    fix: Priority orders

--- conex/nn/priorities.py
+++ conex/nn/priorities.py
@@ -3,29 +3,29 @@ This file specifies the priorities for different behaviors.
 """
 NETWORK_PRIORITIES = {
     "TimeResolution": 1,
-    "Payoff": 2,
-    "NeuroModulator": 100,
+    "Payoff": 100,
+    "NeuroModulator": 120,
 }
 
 NEURON_PRIORITIES = {
-    "NeuronDendrite": 260,
-    "NeuronDynamic": 270,
-    "DirectNoise": 280,
-    "KWTA": 290,
+    "NeuronDendrite": 200,
+    "NeuronDynamic": 220,
+    "DirectNoise": 240,
+    "KWTA": 260,
     "Fire": 300,
     "Trace": 320,
-    "NeuronAxon": 330,
+    "NeuronAxon": 340,
 }
 
-LAYER_PRIORITIES = {"InputDataset": 350}
+LAYER_PRIORITIES = {"InputDataset": 280}
 
 SYNAPSE_PRIORITIES = {
     "Init": 2,
     "WeightInit": 3,
     "SrcDelayInit": 4,
     "DstDelayInit": 5,
-    "DendriticInput": 200,
+    "DendriticInput": 180,
     "LearningRule": 360,
-    "WeightNormalization": 370,
-    "WeightClip": 380,
+    "WeightNormalization": 380,
+    "WeightClip": 400,
 }
