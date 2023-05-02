bab73b32450ac620880b9782e922b03acc5b83ee
atenagm1375 <ashenatena@gmail.com>
2023-05-02 09:31:08 +0330

    edit priority values for easier referencing of occupied values

--- conex/nn/priorities.py
+++ conex/nn/priorities.py
@@ -3,29 +3,29 @@ This file specifies the priorities for different behaviors.
 """
 NETWORK_PRIORITIES = {
     "TimeResolution": 1,
-    "Payoff": 101,
-    "NeuroModulator": 150,
+    "Payoff": 2,
+    "NeuroModulator": 100,
 }
 
 NEURON_PRIORITIES = {
-    "NeuronDendrite": 310,
-    "NeuronDynamic": 320,
-    "DirectNoise": 330,
-    "KWTA": 340,
-    "Fire": 360,
-    "Trace": 380,
-    "NeuronAxon": 390,
+    "NeuronDendrite": 260,
+    "NeuronDynamic": 270,
+    "DirectNoise": 280,
+    "KWTA": 290,
+    "Fire": 300,
+    "Trace": 320,
+    "NeuronAxon": 330,
 }
 
 LAYER_PRIORITIES = {"InputDataset": 350}
 
 SYNAPSE_PRIORITIES = {
-    "Init": 101,
-    "WeightInit": 102,
-    "SrcDelayInit": 103,
-    "DstDelayInit": 104,
-    "DendriticInput": 250,
-    "LearningRule": 420,
-    "WeightNormalization": 430,
-    "WeightClip": 440,
+    "Init": 2,
+    "WeightInit": 3,
+    "SrcDelayInit": 4,
+    "DstDelayInit": 5,
+    "DendriticInput": 200,
+    "LearningRule": 360,
+    "WeightNormalization": 370,
+    "WeightClip": 380,
 }
