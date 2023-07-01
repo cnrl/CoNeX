fix: local2d normalization

conex/behaviors/synapses/specs.py
@@ -132,8 +132,10 @@ class WeightNormalization(Behavior):
     def initialize(self, synapse):
         self.norm = self.parameter("norm", 1)
         self.dims = [x for x in range(1, len(synapse.weights.shape))]
-        if len(synapse.weights.shape) in [1, 2]:
+        if len(synapse.weights.shape) == 2:
             self.dims = [0]
+        if len(synapse.weights.shape) == 3:
+            self.dims = [2]
 
     def forward(self, synapse):
         weights_sum = synapse.weights.sum(dim=self.dims, keepdim=True)
@@ -153,8 +155,10 @@ class CurrentNormalization(Behavior):
     def initialize(self, synapse):
         self.norm = self.parameter("norm", 1)
         self.dims = [x for x in range(1, len(synapse.weights.shape))]
-        if len(synapse.weights.shape) in [1, 2]:
+        if len(synapse.weights.shape) == 2:
             self.dims = [0]
+        if len(synapse.weights.shape) == 3:
+            self.dims = [2]
 
     def forward(self, synapse):
         weights_sum = synapse.weights.sum(dim=self.dims).view(