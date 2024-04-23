51f1d5a30c4a8e72eac1673380c5e1fa16d88393
Amir.H Ebrahimi <ah.ebrahimi@wallex.net>
2024-04-23 15:41:23 +0330

    fix: Change package name for pypi

--- setup.py
+++ setup.py
@@ -4,44 +4,41 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['numpy>=1.23.4', 'torch>=1.13.1', 'pymonntorch>=0.1.0']
+requirements = ["numpy>=1.23.4", "torch>=1.13.1", "pymonntorch>=0.1.0"]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="CNRL",
-    author_email='ashenatena@gmail.com',
-    python_requires='>=3.8',
+    author_email="ashenatena@gmail.com",
+    python_requires=">=3.8",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Cortical Network for everything!",
-    entry_points={
-        'console_scripts': [
-            'conex=conex.cli:main',
-        ],
-    },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='CoNeX',
-    name='CoNeX',
-    packages=find_packages(include=['conex', 'conex.*']),
-    test_suite='tests',
+    keywords="CoNeX",
+    name="CoNeX",
+    packages=["conex"],
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/cnrl/CoNeX',
-    version='0.1.0',
+    url="https://github.com/cnrl/CoNeX",
+    version="0.1.0",
     zip_safe=False,
 )
